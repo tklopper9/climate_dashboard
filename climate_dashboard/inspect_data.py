5896eff5e63e766a15677598be3598e9532f67fe
tklopper9 <t.klopper@raumobil.com>
2025-05-16 15:05:13 +0200

    looking for different plot options

--- climate_dashboard/inspect_data.py
+++ climate_dashboard/inspect_data.py
@@ -14,7 +14,7 @@ def _():
 
 @app.cell
 def _(pd):
-    df = pd.read_csv("data/realistic_ocean_climate_dataset.csv")
+    df = pd.read_csv("climate_dashboard/data/realistic_ocean_climate_dataset.csv")
     df.Date = pd.to_datetime(df.Date)
     df
     return (df,)
@@ -45,25 +45,11 @@ def _(df, px):
 
 @app.cell
 def _(df, px):
-    _d = df[df.Date.dt.year == 2023]
+    _df = df[df.Location == "Red Sea"]
 
-    _df = _df.groupby(["Location"]).aggregate({"Species Observed": "mean", "pH Level": "mean",  "Latitude": "first", "Longitude": "first", "Location": "first"})
+    _fig = px.line(df, x="Date", y="SST (°C)", title='Temperature evolution by Location', color="Location")
 
-    _fig = px.scatter_geo(_df,
-                         lat="Latitude",
-                         lon="Longitude",
-                         size="Species Observed",
-                         text="Location",
-                         title="Number of Species observed in 2015")
     _fig.show()
-    return
-
-
-@app.cell
-def _():
-    _df_2015
-
-
     return
 
 
